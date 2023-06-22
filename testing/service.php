d25374874f6baf0bced074543e953ac49e7b1ae2
AndriuPostre <62949167+AndriuPostre@users.noreply.github.com>
2023-06-22 17:05:18 +0200

    Create service.php

---
+++ testing/service.php
@@ -0,0 +1,9 @@
+<?php
+date_default_timezone_set('Europe/Spain');    
+$DateAndTime = date('YmdHi ', time());  
+echo "$DateAndTime";
+//https://www.delftstack.com/es/howto/php/how-to-get-the-current-date-and-time-in-php/
+//$DateAndTime = date('Y-m-d H:i:s ', time());  
+
+
+?>
